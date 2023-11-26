f0d323e76a9dec4856d3d71a638d276c1ba3158c
Angel Junior Chacaliaza Talla <chacaliazatalla@gmail.com>
2023-11-26 13:59:48 -0500

    Creando menu de ingreso de datos

---
+++ registroNotas.py
@@ -0,0 +1,11 @@
+def main():
+    while True:
+        print("*******************************")
+        print("Bienvenido al registro de notas")
+        print("*******************************")
+        print("Ingrese comando")
+        print("R: Registrar Alumno")
+        print("C: Calificar Alumno")
+        print("P: Promedio de Alumnos")
+        print("S: Suma de notas de Alumnos")
+        print("X: Finalizar")
