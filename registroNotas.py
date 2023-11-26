afa1f977426202a3587311c86ddefd4ba2fb9b4a
Angel Junior Chacaliaza Talla <chacaliazatalla@gmail.com>
2023-11-26 14:02:25 -0500

    Creando elegir opciones

--- registroNotas.py
+++ registroNotas.py
@@ -9,3 +9,26 @@ def main():
         print("P: Promedio de Alumnos")
         print("S: Suma de notas de Alumnos")
         print("X: Finalizar")
+
+        opcion = input("Seleccione una opcion: ")
+
+        if opcion == 'R':
+            pass
+
+        elif opcion =='C':
+            pass
+
+        elif opcion == 'P':
+            pass
+
+        elif opcion == 'S':
+            pass
+
+        elif opcion == 'X':
+            pass
+
+        else:
+            print("Opcion no valida")
+
+if __name__ == "__main__":
+    main()
