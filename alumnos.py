f4fbafbc23990e0ebf9072b5c0194e69c5bb54ff
Angel Junior Chacaliaza Talla <chacaliazatalla@gmail.com>
2023-11-27 10:54:20 -0500

    Creando archivo completo alumnos.py

--- alumnos.py
+++ alumnos.py
@@ -1,3 +1,5 @@
+import pickle
+
 class alumnos:
     def __init__(self, nombre, apellido, edad, nota, nacionalidad):
         self.nombre = nombre
@@ -7,7 +9,26 @@ class alumnos:
         self.nacionalidad = nacionalidad
     
     def leerNota(self):
-        pass
+        return self.nota
 
-    def registrarNota(notaAlumno):
-        pass
+    def registrarNota(self, notaAlumno):
+        #Validamos nota
+        if 0 <= notaAlumno <= 20:
+            self.nota = notaAlumno
+            return True
+        else:
+            print("Error: La nota debe estar en el rango de 0 a 20")
+            return False
+
+#Guardamos archivo usando pickle
+def guardar_alumnos(alumnos, archivo):
+    with open(archivo, 'wb') as f:
+        pickle.dump(alumnos, f)
+
+#Cargamos datos
+def cargar_alumnos(archivo):
+    try:
+        with open(archivo, 'rb') as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return[]
